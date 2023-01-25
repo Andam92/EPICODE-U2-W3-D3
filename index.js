7933179ba2a512cf24491f28c838afc978401c8d
Antonio D'Amico <andam@hotmail.it>
2023-01-26 00:10:26 +0100

    updated JS

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 const display = document.querySelector("p span");
-let secondi = sessionStorage.getItem("secondi")
-  ? sessionStorage.getItem("secondi")
-  : 0;
-let decimi = sessionStorage.getItem("decimi")
-  ? sessionStorage.getItem("decimi")
-  : 0;
 
-/* let secondi;
-if (sessionStorage.getItem("secondi")) {
-  secondi = sessionStorage.getItem("secondi");
+let seconds;
+if (sessionStorage.getItem("seconds")) {
+  seconds = sessionStorage.getItem("seconds");
 } else {
-  secondi = 0;
+  seconds = 0;
 }
 
-let decimi;
+/* let decimi;
 if (sessionStorage.getItem("decimi")) {
-  secondi = sessionStorage.getItem("decimi");
+  seconds = sessionStorage.getItem("decimi");
 } else {
   decimi = 0;
-}
- */
+} */
+
 const counter = () => {
-  decimi += 1;
-  if (decimi == 10) {
-    secondi += 1;
-    sessionStorage.setItem("secondi", secondi);
-    decimi = 0;
-  }
-  sessionStorage.setItem("decimi", decimi);
-  display.innerHTML = `${secondi} : ${decimi}`;
+  seconds += 1;
+  display.innerHTML = `${seconds}`;
+  sessionStorage.setItem("seconds", seconds);
 };
 
-setInterval(counter, 100);
+setInterval(counter, 1000);
